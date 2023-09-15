e42c06032a1e55a5e2623192f5daafa4196b3c84
Vincent <vincemonerow@gmail.com>
2023-09-15 13:35:16 +0100

    Assignment answer in index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+//Differnces between var, let and const
+
+//var
+//var declarations are globally scoped or function scoped, their variables can be updated and re-declared within its scope and var variables are initialized with undefined.
+
+//let
+//let is a blocked scoped, variables can be updated but not re-declared and variables are not initialized and let can be declared without being initialized.
+
+//const is block scoped, variables can neither be updated nor re-declared, variables are not initialized but must be initialized during declaration.
+
+//Data Types 
+// 5 major Data types 
+
+//1. Strings: these are collection of alphanumeric characters and symbols. we store lettrs and words with double quote (""), single quote ('') or a back tick (``). examples are ("Vincent"), ('vincemonerow12@gmail.com') and (`September43`).
+
+//2. Number: These includes decimals and integers for mathematical calculations. Eg. (1247), (12.227).
+
+//3. Booleans: these are data with only two states. eg. true or false, On and off
+
+//4. Undefined: these are variables that has been created but has not been given a value. eg (let rhyme)-We are uncertain about what ryhme is because a value whether a number or string is not decelared.
+
+// 5. Null: This is set intentionally as nothing. it means empty or nothing. Eg. 
+//const rhyme2
+// rhyme2 = null
